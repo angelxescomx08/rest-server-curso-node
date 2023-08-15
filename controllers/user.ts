29b16725fd03f89906fe9a3a44c9a2e8e36ecdf6
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-08-14 18:00:15 -0600

    hacer el hash de la contraseña

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { User } from "../models/user";
+import bcryptjs from "bcryptjs";
 
 export const userGET = (req: Request, res: Response) => {
   const { page = 1, limit = 100 } = req.query;
@@ -11,11 +12,18 @@ export const userGET = (req: Request, res: Response) => {
 };
 
 export const userPOST = async (req: Request, res: Response) => {
-  const body = req.body;
-  const user = new User(body);
+  const { name, email, password, rol } = req.body;
+  const user = new User({
+    name,
+    email,
+    password,
+    rol,
+  });
+  const salt = bcryptjs.genSaltSync();
+  user.password = bcryptjs.hashSync(password, salt);
   await user.save();
   res.status(201).json({
-    body,
+    user,
   });
 };
 
